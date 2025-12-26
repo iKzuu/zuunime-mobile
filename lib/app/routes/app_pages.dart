bd17f9614041e51a6b3b343a88796c9a00b61765
iKzuu <anangsetiaji30@gmail.com>
2025-12-26 18:13:10 +0700

    add new page

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -3,6 +3,7 @@ import 'package:zuunimelist/app/bindings/AnimeBinding.dart';
 import 'package:zuunimelist/app/bindings/AnimeDetailBinding.dart';
 import 'package:zuunimelist/app/bindings/AnimeSearchBinding.dart';
 import 'package:zuunimelist/app/modules/views/anime_detail_view.dart';
+import 'package:zuunimelist/app/modules/views/character_and_va_view.dart';
 import 'package:zuunimelist/app/modules/views/home.dart';
 import 'package:zuunimelist/components/widget/widget_wrapper.dart';
 
@@ -19,6 +20,11 @@ class AppPages {
     GetPage(
       name: Routes.ANIME_DETAIL,
       page: () => AnimeDetailView(),
+      binding: AnimeDetailBinding(),
+    ),
+    GetPage(
+      name: Routes.ALL_CHARACTERS_VA,
+      page: () => CharacterAndVoiceActor(),
       binding: AnimeDetailBinding(),
     ),
   ];
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -4,4 +4,5 @@ class Routes {
   static const WIDGET_WRAPPER = "/main";
   static const HOME = "/home";
   static const ANIME_DETAIL = "/anime-detail";
+  static const ALL_CHARACTERS_VA = "/all-characters-va";
 }
